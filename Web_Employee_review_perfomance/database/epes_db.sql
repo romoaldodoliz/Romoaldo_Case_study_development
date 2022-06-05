62219de230e8a3f1a3453e08ed5f2b72c988a861
Romoaldo Doliz <63649156+romoaldodoliz@users.noreply.github.com>
2022-06-05 21:47:56 +0200

    Standard Bank Case Study
    This is my case study repository

---
+++ Web_Employee_review_perfomance/database/epes_db.sql
@@ -0,0 +1,361 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2022 at 09:40 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `epes_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department_list`
+--
+
+CREATE TABLE `department_list` (
+  `id` int(30) NOT NULL,
+  `department` varchar(200) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department_list`
+--
+
+INSERT INTO `department_list` (`id`, `department`, `description`) VALUES
+(1, 'IT Department', 'Information Technology Department'),
+(2, 'Sample  Departement', 'Sample Only');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation_list`
+--
+
+CREATE TABLE `designation_list` (
+  `id` int(30) NOT NULL,
+  `designation` varchar(200) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `designation_list`
+--
+
+INSERT INTO `designation_list` (`id`, `designation`, `description`) VALUES
+(1, 'Sr. Programmer', 'Senior Programmer'),
+(2, 'Jr. Programmer', 'Junior Programmer'),
+(3, 'Project Manager', 'Project Manager'),
+(4, 'QA/QC Analyst', 'Quality Assurance and Quality Control Analyst');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_list`
+--
+
+CREATE TABLE `employee_list` (
+  `id` int(30) NOT NULL,
+  `employee_id` varchar(50) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `department_id` int(30) NOT NULL,
+  `designation_id` int(30) NOT NULL,
+  `evaluator_id` int(30) NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee_list`
+--
+
+INSERT INTO `employee_list` (`id`, `employee_id`, `firstname`, `middlename`, `lastname`, `email`, `password`, `department_id`, `designation_id`, `evaluator_id`, `avatar`, `date_created`) VALUES
+(1, '', 'Teste', '', 'Bank', 'teste1@sample.com', 'b59c67bf196a4758191e42f76670ceba', 1, 2, 1, '1607134500_avatar.jpg', '2020-12-05 10:15:38'),
+(2, '', 'Teste', '', 'Standard', 'teste@sample.com', 'a88df23ac492e6e2782df6586a0c645f', 1, 2, 1, '1654332420_pngegg.png', '2021-03-02 13:52:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluator_list`
+--
+
+CREATE TABLE `evaluator_list` (
+  `id` int(30) NOT NULL,
+  `employee_id` varchar(50) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `evaluator_list`
+--
+
+INSERT INTO `evaluator_list` (`id`, `employee_id`, `firstname`, `middlename`, `lastname`, `email`, `password`, `avatar`, `date_created`) VALUES
+(1, '', 'Claire', '', 'Blake', 'cblake@sample.com', 'b59c67bf196a4758191e42f76670ceba', '1607136060_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-05 10:41:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int(30) NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `evaluator_id` int(30) NOT NULL,
+  `efficiency` int(11) NOT NULL,
+  `timeliness` int(11) NOT NULL,
+  `quality` int(11) NOT NULL,
+  `accuracy` int(11) NOT NULL,
+  `remarks` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ratings`
+--
+
+INSERT INTO `ratings` (`id`, `employee_id`, `task_id`, `evaluator_id`, `efficiency`, `timeliness`, `quality`, `accuracy`, `remarks`, `date_created`) VALUES
+(2, 1, 1, 1, 5, 4, 5, 5, 'Sample', '2020-12-05 15:18:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Employee Performance Evaluation System', 'romoaldodoliz@hotmail.com', '+258 862020616', 'Cidade da Matola', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `due_date` date NOT NULL,
+  `completed` date NOT NULL,
+  `status` int(1) NOT NULL COMMENT '0=pending, 1=on-progress,3=Completed',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `task_list`
+--
+
+INSERT INTO `task_list` (`id`, `task`, `description`, `employee_id`, `due_date`, `completed`, `status`, `date_created`) VALUES
+(1, 'Sample Task 1', '																					Sample Only																		', 1, '2020-12-02', '0000-00-00', 2, '2020-12-05 11:06:15'),
+(2, 'Sample Task 2', '														&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida, magna eu sagittis venenatis, lorem tellus mollis tellus, viverra facilisis metus odio vel dui. Sed posuere interdum ultrices. Pellentesque blandit enim in condimentum pretium. Suspendisse id tortor sit amet augue rutrum condimentum. Fusce ac mattis purus, eget vehicula sem. Maecenas sit amet orci id lorem tristique tempor. Nullam iaculis quis velit at dapibus. Nullam scelerisque, metus vitae feugiat aliquam, risus turpis pellentesque justo, vitae varius urna leo vitae nisl. Pellentesque viverra ipsum et diam blandit varius. Suspendisse blandit ex vitae hendrerit volutpat. Nulla fermentum dolor at lorem accumsan, nec lacinia mi pellentesque. Mauris ac augue vel elit lobortis maximus.&lt;/span&gt;																									', 1, '2020-12-24', '0000-00-00', 0, '2020-12-05 13:09:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_progress`
+--
+
+CREATE TABLE `task_progress` (
+  `id` int(11) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `progress` text NOT NULL,
+  `is_complete` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0=no,1=Yes',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `task_progress`
+--
+
+INSERT INTO `task_progress` (`id`, `task_id`, `progress`, `is_complete`, `date_created`) VALUES
+(1, 1, '&lt;p&gt;Sample Progress&lt;/p&gt;', 0, '2020-12-05 11:29:48'),
+(2, 1, '&lt;p&gt;Sample Progress&lt;/p&gt;', 0, '2020-12-05 11:32:15'),
+(3, 1, '&lt;p&gt;Sample 2&lt;/p&gt;', 0, '2020-12-05 11:34:18'),
+(4, 1, 'asdasd', 0, '2020-12-05 11:34:31'),
+(5, 1, '&lt;p&gt;complete&lt;/p&gt;', 1, '2020-12-05 11:54:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `avatar`, `date_created`) VALUES
+(1, 'Romoaldo', 'Doliz', 'romoaldodoliz@hotmail.com', 'b59c67bf196a4758191e42f76670ceba', '1654332360_avatar red.jpg', '2020-11-26 10:57:04'),
+(2, 'Claire', 'Blake', 'cblake@sample.com', 'cd74fae0a3adf459f73bbf187607ccea', 'no-image-available.png', '2021-03-02 13:53:17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department_list`
+--
+ALTER TABLE `department_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `designation_list`
+--
+ALTER TABLE `designation_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee_list`
+--
+ALTER TABLE `employee_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `evaluator_list`
+--
+ALTER TABLE `evaluator_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_progress`
+--
+ALTER TABLE `task_progress`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department_list`
+--
+ALTER TABLE `department_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `designation_list`
+--
+ALTER TABLE `designation_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `employee_list`
+--
+ALTER TABLE `employee_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `evaluator_list`
+--
+ALTER TABLE `evaluator_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `task_progress`
+--
+ALTER TABLE `task_progress`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
